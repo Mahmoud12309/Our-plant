51bb4e133f13998eeb31b1c630dfbe020bfec0e7
Mahmoud12309 <122573299+Mahmoud12309@users.noreply.github.com>
2023-07-31 13:01:39 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 let bar = document.querySelector(".bar"),
-nav = document.querySelector(".nav");
+nav = document.querySelector(".nav"),
+input_search = document.querySelector(".ser-b"),
+input_i = document.querySelector(".ser-i");
 bar.addEventListener('click',() => {
   bar.classList.toggle("active");  
   nav.classList.toggle("active");  
+});
+
+input_i.addEventListener('click',() => {
+  if(input_search.value !== ""){
+    if (input_search.value === "home" || input_search.value === "about" || input_search.value === "services" || input_search.value === "blog"){
+      window.location.href =`#${input_search.value}`;
+    }
+  }else{
+
+  }
 });
